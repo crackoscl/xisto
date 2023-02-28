b0fd9238f0c5d5b8af724be234f879a801e51a8e
crackoscl <gilbert.ata@gmail.com>
2023-02-28 00:37:18 -0300

    se agrega Schema basico

--- app/urls.py
+++ app/urls.py
@@ -1,5 +1,6 @@
 
 from django.urls import include, path
+from rest_framework.schemas import get_schema_view
 # from rest_framework import routers
 from .views import Principal,DancingListApiView,DacingCreateApiView,DacingDestroyApiview,DacingRetriApiView,DacingUpdateApiview
 
@@ -14,8 +15,13 @@ from .views import Principal,DancingListApiView,DacingCreateApiView,DacingDestro
 # ]
 
 urlpatterns = [
-     path('',Principal.as_view(),name='principal'),
-     path('api/',DancingListApiView.as_view(),name='list_dacing'),
+    path('',Principal.as_view(),name='principal'),
+    path('api/', get_schema_view(
+        title="Xisto pruebas",
+        description="API for all things …",
+        version="1.0.0"
+    ), name='openapi-schema'),
+     path('api/list_dacing/',DancingListApiView.as_view(),name='list_dacing'),
      path('api/create_dacing/',DacingCreateApiView.as_view(),name='create_dacing'),
      path('api/retriview_dancing/<str:pk>/',DacingRetriApiView.as_view(),name='retriview_dacing'),
      path('api/update_dancing/<str:pk>/',DacingUpdateApiview.as_view(),name='update_dacing'),
