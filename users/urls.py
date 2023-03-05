5acf1e6479e6135e90d046382b989c0b8f520ba2
crackoscl <gilbert.ata@gmail.com>
2023-03-05 13:43:14 -0300

    refresh token

--- users/urls.py
+++ users/urls.py
@@ -8,11 +8,11 @@ app_name = 'users'
 
 
 urlpatterns = [
-    path('get_resfresh_token',refresh_get(),name='get_resfresh_token'),
-    path('token_refresh',TokenRefresh.as_view(),name='TokenRefresh'),
-    path('login/', Login.as_view(), name='login'),
-    path('logout/', Logout.as_view(), name='logout'),
-    path('signup/', Signup.as_view(), name='signup'),
-    path('changepassword/', UpdatePassword.as_view(), name='change_pasword'),
+    path('get_resfresh_token',refresh_get,name='get_resfresh_token'), #obtiene el token de refresh
+    path('token_refresh',TokenRefresh.as_view(),name='TokenRefresh'), # obtiene un nuevo access
+    path('login/', Login.as_view(), name='login'), #genera token de acces y refresh
+    path('logout/', Logout.as_view(), name='logout'), # remueve la cookie 
+    path('signup/', Signup.as_view(), name='signup'), # crea un nuevo usuario y te envia un link token al correo
+    path('changepassword/', UpdatePassword.as_view(), name='change_pasword'), # cambia la password
 ]
 
